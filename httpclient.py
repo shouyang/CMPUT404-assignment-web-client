62c4444585027e0b7b174efdaead8b3bd993a4bb
Shouyang Zhou <shouyang@ualberta.ca>
2019-02-04 11:43:32 -0700

    Stuf on VM Desktop

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,36 +23,41 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
-import urlparse
+import urllib.parse as urlparse
+
 
 def help():
-    print "httpclient.py [GET/POST] [URL]\n"
+    print ("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
     #def get_host_port(self,url):
-    GET_REQUEST_TEMPLATE = "GET {path} HTTP/1.1\r\n" + "Host:{host}:{port}\r\n" + "Content-Length:0\r\n" + "\r\n"
-    POST_REQUEST_TEMPLATE = "POST {path} HTTP/1.1\r\n" + "Host:{host}:{port}\r\n" + "Content-Length:{content_length}\r\n" + "Content-Type:{content_type}\r\n" + "\r\n" + "{body}"
+    GET_REQUEST_TEMPLATE = "GET {path} HTTP/1.1\r\n" + \
+        "Host:{host}:{port}\r\n" + "Content-Length:0\r\n" + "\r\n"
+    POST_REQUEST_TEMPLATE = "POST {path} HTTP/1.1\r\n" + "Host:{host}:{port}\r\n" + \
+        "Content-Length:{content_length}\r\n" + \
+        "Content-Type:{content_type}\r\n" + "\r\n" + "{body}" + "\r\n"
     ENCODING = "utf-8"
-
 
     def connect(self, host, port):
         # use sockets
         self.sock = socket.socket()
-        self.sock.connect( (host,port) )
+        self.sock.connect((host, port))
 
     def get_code(self, data):
-        return data.split("\r\n")[0].split(" ")[1] 
+        return data.split("\r\n")[0].split(" ")[1]
 
-    def get_headers(self,data):
+    def get_headers(self, data):
         return data.split("\r\n\r\n")[0]
 
     def get_body(self, data):
-        return data.split("\r\n\r\n").pop()
+        return data.split("\r\n\r\n")[-1]
 
     # read everything from the socket
     def recvall(self, sock):
@@ -64,7 +69,7 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        return str(buffer)
+        return str(buffer, self.ENCODING)
 
     def GET(self, url, args=None):
 
@@ -78,19 +83,21 @@ class HTTPClient(object):
         if not path:
             path = "/"
 
-        self.connect(host,port)
+        self.connect(host, port)
 
-        self.sock.sendall(bytearray(self.generateGETRequest(path,host,port), self.ENCODING))
+        self.sock.sendall(
+            bytearray(self.generateGETRequest(path, host, port), self.ENCODING))
         data = self.recvall(self.sock)
 
         code = int(self.get_code(data))
         body = str(self.get_body(data))
 
-        print(data)
+        self.sock.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        
+
         host = urlparse.urlparse(url).hostname
         port = urlparse.urlparse(url).port
         path = urlparse.urlparse(url).path
@@ -101,45 +108,42 @@ class HTTPClient(object):
         if not path:
             path = "/"
 
-        self.connect(host,port)
+        self.connect(host, port)
 
         if args:
-            body = urllib.urlencode(args);
+            body = urlparse.urlencode(args, encoding="utf-8")
             content_length = len(body)
-            content_type   = "application/x-www-form-urlencoded"
+            content_type = "application/x-www-form-urlencoded"
 
         else:
             body = ""
             content_length = 0
-            content_type   = "application/x-www-form-urlencoded"
+            content_type = "application/x-www-form-urlencoded"
 
-        print("======")
-        print(self.generatePOSTRequest(path,host,port, content_length , content_type, body))
-        print("======")
-        
-        self.sock.sendall(bytearray(self.generatePOSTRequest(path,host,port, content_length , content_type, body), self.ENCODING))
+        self.sock.sendall(bytearray(self.generatePOSTRequest(
+            path, host, port, content_length, content_type, body), self.ENCODING))
+     
+     
         data = self.recvall(self.sock)
-
-        print("+++++")
-        print(data)
-        print("+++++")
 
         code = int(self.get_code(data))
         body = str(self.get_body(data))
 
+        self.sock.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
-    def generateGETRequest(self, path, host, port):
-        return self.GET_REQUEST_TEMPLATE.format(path = path, host = host, port = port)
+            return self.GET(url, args)
 
-    def generatePOSTRequest(self,path, host, port, content_length, content_type, body):
-        return self.POST_REQUEST_TEMPLATE.format(path = path, host = host, port = port, content_length = content_length, content_type = content_type, body = body)
+    def generateGETRequest(self, path, host, port):
+        return self.GET_REQUEST_TEMPLATE.format(path=path, host=host, port=port)
+
+    def generatePOSTRequest(self, path, host, port, content_length, content_type, body):
+        return self.POST_REQUEST_TEMPLATE.format(path=path, host=host, port=port, content_length=content_length, content_type=content_type, body=body)
+
 
 if __name__ == "__main__":
     client = HTTPClient()
@@ -148,6 +152,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print client.command( sys.argv[2], sys.argv[1] )
+        print( client.command(sys.argv[2], sys.argv[1]))
     else:
-        print client.command( sys.argv[1] )   
+        print(client.command(sys.argv[1]))
